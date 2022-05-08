98735f835f6d7476024dc1705963a6776cbf926e
danhdeng <hui_danny_deng@hotmail.com>
2022-05-08 17:19:50 -0400

    added register page

--- client/pages/auth/register.tsx
+++ client/pages/auth/register.tsx
@@ -1,10 +1,165 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBCardFooter,
+  MDBValidation,
+  MDBBtn,
+  MDBIcon,
+  MDBSpinner,
+  MDBNavbarLink,
+  MDBValidationItem,
+} from "mdb-react-ui-kit";
+import Link from 'next/link';
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { register } from "../../redux/features/authSlice";
+import { useRouter } from "next/router";
 
-/**
- *
- * @return {React.Component}
- * return register page
- */
-export default function RegisterPage() {
-  return <div>register Page</div>;
-}
+const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const Register = () => {
+  const [formValue, setFormValue] = useState(initialState);
+  const { loading, error } = useSelector((state:any) => ({ ...state.auth }));
+  const { email, password, firstName, lastName, confirmPassword } = formValue;
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  useEffect(() => {
+    error && toast.error(error);
+  }, [error]);
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      return toast.error("Password should match");
+    }
+    if (email && password && firstName && lastName && confirmPassword) {
+      // @ts-ignore: skip the type checking
+      dispatch(register({ formValue, router, toast }));
+    }
+  };
+  const onInputChange = (e:any) => {
+    const { name, value } = e.target;
+    setFormValue({ ...formValue, [name]: value });
+  };
+  return (
+    <div
+      style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "450px",
+        alignContent: "center",
+        marginTop: "120px",
+      }}
+    >
+      <MDBCard alignment="center">
+        <MDBIcon fas icon="user-circle" className="fa-2x" />
+        <h5>Sign Up</h5>
+        <MDBCardBody>
+          <MDBValidation onSubmit={handleSubmit} noValidate className="row g-3">
+            <MDBValidationItem
+              feedback="Please provide first name"
+              invalid
+              className="col-md-6"
+            >
+              <MDBInput
+                label="First Name"
+                type="text"
+                value={firstName}
+                name="firstName"
+                onChange={onInputChange}
+                required
+              />
+            </MDBValidationItem>
+            <MDBValidationItem
+              feedback="Please provide last name"
+              invalid
+              className="col-md-6"
+            >
+              <MDBInput
+                label="Last Name"
+                type="text"
+                value={lastName}
+                name="lastName"
+                onChange={onInputChange}
+                required
+              />
+            </MDBValidationItem>
+            <MDBValidationItem
+              feedback="Please provide email"
+              invalid
+              className="col-md-12"
+            >
+              <MDBInput
+                label="Email"
+                type="email"
+                value={email}
+                name="email"
+                onChange={onInputChange}
+                required
+              />
+            </MDBValidationItem>
+            <MDBValidationItem
+              feedback="Please provide password"
+              invalid
+              className="col-md-12"
+            >
+              <MDBInput
+                label="Password"
+                type="password"
+                value={password}
+                name="password"
+                onChange={onInputChange}
+                required
+              />
+            </MDBValidationItem>
+            <MDBValidationItem
+              feedback="Please provide confirm password"
+              invalid
+              className="col-md-12"
+            >
+                <MDBInput
+                  label="Password Confirm"
+                  type="password"
+                  value={confirmPassword}
+                  name="confirmPassword"
+                  onChange={onInputChange}
+                  required
+                />
+            </MDBValidationItem>
+            <div className="col-12">
+              <MDBBtn style={{ width: "100%" }} className="mt-2">
+                {loading && (
+                  <MDBSpinner
+                    size="sm"
+                    role="status"
+                    tag="span"
+                    className="me-2"
+                  />
+                )}
+                Register
+              </MDBBtn>
+            </div>
+          </MDBValidation>
+        </MDBCardBody>
+        <MDBCardFooter>
+          <Link href="/auth/login" passHref>
+            <MDBNavbarLink active aria-current="page">
+              Already have an account ? Sign In
+            </MDBNavbarLink>
+          </Link>
+        </MDBCardFooter>
+      </MDBCard>
+    </div>
+  );
+};
+
+export default Register;
